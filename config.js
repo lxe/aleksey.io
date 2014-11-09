8a6ce1289c06de3bab01bc2724a9c9748142615e
Aleksey Smolenchuk <lxe@lxe.co>
2014-11-09 12:28:04 -0800

    config change

--- config.js
+++ config.js
@@ -1,21 +1,35 @@
 var path = require('path');
+var deepExtend = require('deep-extend');
+
+var defaults = {
+  paths: {
+    contentPath: path.join(__dirname, 'content'),
+  },
+  database: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, 'content/data/aleksey.io.db')
+    }
+  }
+};
 
 var config = module.exports = {
-  development: {
-    paths: {
-      contentPath: path.join(__dirname, 'content'),
-    },
+
+  development: deepExtend(defaults, {
     url: 'http://127.0.0.1:3000',
-    database: {
-      client: 'sqlite3',
-      connection: {
-        filename: path.join(__dirname, 'content/data/aleksey.io.db')
-      }
-    },
     server: {
       host: '127.0.0.1',
       port: '3000'
     },
     logging: false
-  }
+  }),
+
+  production: deepExtend(defaults, {
+    url: 'http://aleksey.io',
+    server: {
+      host: '127.0.0.1',
+      port: '3080'
+    }
+  })
+
 };
